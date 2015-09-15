2a42ed771c27adcf153367dfae2f562abde71d40
REAGAN ROSE <reaganmro@gmail.com>
2015-09-15 09:26:35 -0400

    Add data

--- Section1.R
+++ Section1.R
@@ -1,7 +1,7 @@
 #######################
 # Section 1, Stat 139 #
 #     Intro to R      #
-#      9/9/15         #
+#      9/10/15        #
 #    Reagan Rose      #
 #######################
 
@@ -23,7 +23,7 @@ round(pi,0); round(pi, 1); round(pi,4)
 ### Matrices ###
 matrix(c(1,2,3,4,5,6), byrow=T, nrow=3)
 A <- matrix(c(1,2,3,4,5,6), byrow=T, nrow=3) # name the matrix A
-B <- matrix(c(7,8,9,10,11,12), byrow=T, ncol=2)
+B = matrix(c(7,8,9,10,11,12), byrow=T, ncol=2)
 t(A) # transpose
 A*B # multiply the numbers in the matrix
 t(A) %*% B # multiply the actual matrices
@@ -45,7 +45,7 @@ x + y
 
 x <- 1:10
 y <- seq(1,10)
-z <- seq(1,10,0.1)
+z <- seq(from=1,to=10,by=0.1)
 w <- rep(1,10)
 
 # Control Flow and Loops
@@ -70,12 +70,13 @@ if (x > 10000){
 }
 
 ### Inputting Data ###
-setwd("~/Desktop/Harvard/Teaching/Stat 139/R Code")
+setwd("~/Desktop/Harvard/Teaching/Stat 139/R Code and Data")
 crabs <- read.csv("crabs.csv")
 
 head(crabs) # check out the dataset, default is 6 obs
 head(crabs, n=10)
 tail(crabs)
+dim(crabs)
 
 crabs <- read.table("crabs.dat", header=T)
 names(crabs)
@@ -86,6 +87,8 @@ crabs$width
 crabs[,3]
 
 x <- crabs$width
+
+is.na(x) # Check if there are any missing values in the variable x
 
 mean(x)
 sd(x)
@@ -102,22 +105,34 @@ cor(x,y)
 crabs3 <- subset(crabs, crabs$color == 3)
 head(crabs3)
 
+crabs[1:10,]
+crabs[,2]
+crabs[1:2, 3:4]
 crabs3.2 <- crabs[crabs$color == 3,]
 
+#crabs[rows, columns]
 
 ### Regression ###
-fit <- lm(y ~ x)
-summary(fit)
+y <- crabs$satell
+x <- crabs$weight
+z <- crabs$width
+
+fit1 <- lm(y ~ x)
+fit2 <- lm(y ~ x+z) # To include a predictor use '+'
+fit3 <- lm(crabs$satell~., data=crabs) # To include all variables as predictors use '.'
+
+summary(fit1)
+summary(fit2)
 
 ### Graphics and Plotting ###
-hist(crabs$width)
+hist(crabs$width, main="Histogram of width")
 hist(crabs$width, breaks=25)
 
 plot(crabs$width, crabs$satell)
 plot(crabs$satell~crabs$width)
 
 # Adding lines to your plot
-fit <- lm(crabs$satell, crabs$width)
+fit <- lm(crabs$satell~crabs$width)
 plot(crabs$width, crabs$satell)
 abline(fit)
 
@@ -125,7 +140,7 @@ abline(fit)
 # lty indicates the texture of the line
 # lwd is the weight of the line (higher numbers = wider line)
 plot(crabs$width, crabs$satell, main="Our First Plot")
-abline(v=mean(crabs$width), lty=2) 
+abline(v=mean(crabs$width), lty=2, lwd=2) 
 abline(v=mean(crabs$width) + 2 * sd(crabs$width), lty=3) 
 abline(v=mean(crabs$width) - 2 * sd(crabs$width), lty=3)
 abline(h=max(crabs$satell), lty=3, col="blue", lwd=2)
@@ -135,9 +150,9 @@ colors()
 
 
 # Tables and Barplots
-table(crabs$color)
+table(color)
 table(crabs$color, crabs$spine)
-barplot(table(crabs$satell), name="$ of Satellites")
+barplot(table(crabs$satell), name="# of Satellites")
 
 # Scatterplot and Correlation Matrix
 attach(crabs) 
@@ -181,8 +196,9 @@ confint(lr)
 
 data$quality = as.numeric(data$quality)
 attach(data)
-goodwines = data[quality==1,]
-badwines = data[quality==2,]
+goodwines = subset(data, data$quality=="good")
+goodwines = data[quality=="good",]
+badwines = data[quality=="not good",]
 
 # Is there significantly different alcohol content in the good wines?
 t.test(goodwines$alcohol, badwines$alcohol, alternative=c("two.sided"))
