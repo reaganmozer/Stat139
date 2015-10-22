56e16666fe3a0c1b5c619786125332f76ab0fc24
REAGAN ROSE <reaganmro@gmail.com>
2015-10-21 20:27:55 -0400

    Section 7 Materials Updated

--- Section 7/Section7.R
+++ Section 7/Section7.R
@@ -1,19 +1,19 @@
 #######################
 # Section 7, Stat 139 #
 #       ANOVA II      #
-#      10/21/15       #
+#      10/22/15       #
 #    Reagan Rose      #
 #######################
 
 #### EXERCISE 1 ####
-a = c(4,3.5,5,3,2)
-b = c(3,2,0,2,2)
-c = c(1,0,1,2,1)
-means = c(mean(a), mean(b), mean(c))
-sds = c(sd(a),sd(b),sd(c))
+group1 = c(4,3.5,5,3,2)
+group2 = c(3,2,0,2,2)
+group3 = c(1,0,1,2,1)
+means = c(mean(group1), mean(group2), mean(group3))
+sds = c(sd(group1),sd(group2),sd(group3))
 n=c(5,5,5)
 
-vals = c(a,b,c)
+vals = c(group1,group2,group3)
 groups = c(rep(1,5), rep(2,5), rep(3,5))
 
 # Run the ANOVA
@@ -22,9 +22,9 @@ model = aov(data$vals~factor(data$groups))
 summary(model)
 
 # Pairwise t-tests
-t.test(a,b, pooled.var=T)$p.value*3
-t.test(a,c, pooled.var=T)$p.value*3
-t.test(b,c, pooled.var=T)$p.value*3
+t.test(group1,group2, pooled.var=T)$p.value*3
+t.test(group1,group3, pooled.var=T)$p.value*3
+t.test(group2,group3, pooled.var=T)$p.value*3
 
 # Contrast tests
 Sp2 = sum((n-1)*sds^2)/ (sum(n)-length(n))
@@ -60,7 +60,20 @@ p.val*3
 # Tukey's method
 TukeyHSD(model) # the easiest way
 
-# the hard way
+# the harder way
+numgroups = 3
+nobs = 15
+alpha = 0.05
+
+q = qtukey(1-alpha,numgroups,nobs - numgroups)/sqrt(2)
+alpha_tukey=2*(1-pt(q,df=nobs-numgroups))
+
+# APPROXIMATION to the Tukey
+t.test(group1,group2,pooled.var=T)$p.value
+t.test(group1,group3,pooled.var=T)$p.value
+t.test(group2,group3,pooled.var=T)$p.value
+
+# the hardest way
 nbar = mean(n)
 nobs = 15
 I = 3
@@ -79,12 +92,25 @@ t_obs3 = (means[2]-means[3])/sqrt(Sp2/nbar)
 
 # Simulate some data
 
-n.sim = 1000
+n.sim = 100
+
+# Initalize some vectors where we'll put our simulation values
 pval_12.t = pval_13.t = pval_23.t = c() #t-tests
 pval_12.c = pval_13.c = pval_23.c = c() #contrast tests
 pval_12.HSD = pval_13.HSD = pval_23.HSD = c() #Tukey's HSD
 
+# Initialize all of our given numbers
+alpha = 0.05
+n = 15
+numgroups = 3
+numtests = choose(numgroups,2)
+alpha_bonferroni = alpha / numtests
+
+q = qtukey(1-alpha,numgroups,n - numgroups)/sqrt(2)
+alpha_tukey=2*(1-pt(q,df=n-numgroups))
+
 for (i in 1:n.sim){
+# First we generate data from 3 groups
 x1 = rnorm(5, mean=1, sd=1)
 x2 = rnorm(5, mean=1, sd=1)
 x3 = rnorm(5, mean=1, sd=1)
@@ -94,9 +120,10 @@ groups = rep(1:3, each=5)
 k = 3
 
 # Pairwise t-tests
-pval_12.t[i]=min(t.test(x1,x2,var.equal=T)$p.value * k,1) #multiply by k to correct for multiple comparisons
-pval_13.t[i]=min(t.test(x1,x3,var.equal=T)$p.value * k,1)
-pval_23.t[i]=min(t.test(x2,x3, var.equal=T)$p.value * k,1)
+alpha = 0.05
+pval_12.t[i]=t.test(x1,x2,var.equal=T)$p.value #multiply by k to correct for multiple comparisons
+pval_13.t[i]=t.test(x1,x3,var.equal=T)$p.value
+pval_23.t[i]=t.test(x2,x3, var.equal=T)$p.value
 
 # Contrast tests
 means = c(mean(x1), mean(x2), mean(x3))
@@ -109,25 +136,21 @@ a = c(1,-1,0)
 T_num = sum(a*means)
 T_denom = sqrt(Sp2) * sqrt(sum(a^2/n))
 T = abs(T_num / T_denom)
-p.val = 2*(1-pt(T,df))
+pval_12.c[i] = 2*(1-pt(T,df))
 
-pval_12.c[i]=min(p.val*3,1)
 
 a = c(1,0,-1)
 T_num = sum(a*means)
 T_denom = sqrt(Sp2) * sqrt(sum(a^2/n))
 T = abs(T_num / T_denom)
-p.val = 2*(1-pt(T,df))
+pval_13.c[i] = 2*(1-pt(T,df))
 
-pval_13.c[i]=min(p.val*3,1)
 
 a = c(0,1,-1)
 T_num = sum(a*means)
 T_denom = sqrt(Sp2) * sqrt(sum(a^2/n))
 T = abs(T_num / T_denom)
-p.val = 2*(1-pt(T,df))
-
-pval_23.c[i]=min(p.val*3,1)
+pval_23.c[i] = 2*(1-pt(T,df))
 
 # Tukey's HSD
 model = aov(x~factor(groups))
@@ -146,9 +169,10 @@ abline(v=0.05, col="red")
 hist(pval_12.HSD)
 abline(v=0.05, col="red")
 
-mean(pval_12.t <= 0.05)
-mean(pval_12.c <= 0.05)
-mean(pval_12.HSD <= 0.05)
+# Compares Type I Error rate for mean difference between groups 1 and 2
+mean(pval_12.t <= alpha_bonferroni)
+mean(pval_12.c <= alpha_bonferroni)
+mean(pval_12.HSD <= alpha)
 
 par(mfrow=c(1,3))
 hist(pval_13.t)
@@ -158,9 +182,11 @@ abline(v=0.05, col="red")
 hist(pval_13.HSD)
 abline(v=0.05, col="red")
 
-mean(pval_13.t <= 0.05)
-mean(pval_13.c <= 0.05)
-mean(pval_13.HSD <= 0.05)
+# Compares Type I Error rate for mean difference between groups 1 and 3
+mean(pval_13.t <= alpha_bonferroni)
+mean(pval_13.c <= alpha_bonferroni)
+mean(pval_13.HSD <= alpha)
+
 
 
 par(mfrow=c(1,3))
@@ -171,7 +197,8 @@ abline(v=0.05, col="red")
 hist(pval_23.HSD)
 abline(v=0.05, col="red")
 
-mean(pval_23.t <= 0.05)
-mean(pval_23.c <= 0.05)
-mean(pval_23.HSD <= 0.05)
+# Compares Type I Error rate for mean difference between groups 2 and 3
+mean(pval_23.t <= alpha_bonferroni)
+mean(pval_23.c <= alpha_bonferroni)
+mean(pval_23.HSD <= alpha)
 par(mfrow=c(1,1))
